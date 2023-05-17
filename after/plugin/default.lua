d39df116cb4fbde7dd5ea8310628310bb7fd93a4
Seth Horsley <isethi@me.com>
2023-05-17 20:42:35 +0200

    Add neogit, vim-surround plugins Add key remaps

--- after/plugin/default.lua
+++ after/plugin/default.lua
@@ -2,17 +2,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
--- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-
-local git_files = function()
-  require('telescope.builtin').git_files({ git_command = {"git","ls-files","-m"}})
-  -- require('telescope.builtin').git_files({ git_command = {"nvim_ggs_func"}})
-  -- require('telescope.builtin').git_files({ show_untracked=true, git_command = {"git", "status", "--porcelain", "|", "sed", "s/^...//"}})
-end
-
--- vim.keymap.set('n', '<leader>ff', require('telescope.builtin').git_files({git_command = {"git", "status", "--porcelain", "|", "sed", "s/^...//"}}), {desc = 'Search [G]it [F]iles'})
-vim.keymap.set('n', '<leader>ff', git_files, {desc = 'Search [G]it [F]iles'})
-
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').git_status, {desc = 'Search [G]it [F]iles'})
 
 vim.keymap.set({"n", "v", "i"}, "<C-a>", "<Home>")
 vim.keymap.set({"n", "v", "i"}, "<C-e>", "<End>")
---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,21 @@
+return {
+  'TimUntersberger/neogit',
+  requires = 'nvim-lua/plenary.nvim',
+  setup = function()
+    require('neogit').setup {
+      disable_signs = false,
+      disable_context_highlighting = false,
+      disable_commit_confirmation = false,
+      -- customize displayed signs
+      signs = {
+        -- { CLOSED, OPENED }
+        section = { ">", "v" },
+        item = { ">", "v" },
+        hunk = { "", "" },
+      },
+      integrations = {
+        diffview = true
+      }
+    }
+  end
+}
