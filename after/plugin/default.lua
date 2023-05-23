3eca7f9928bb8bc53603c216b9d0d4dbb0ad7ae5
Seth Horsley <isethi@me.com>
2023-05-23 15:32:10 +0200

    add keyremaps

--- after/plugin/default.lua
+++ after/plugin/default.lua
@@ -4,8 +4,12 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').git_status, {desc = 'Search [G]it [F]iles'})
 
-vim.keymap.set({"n", "v", "i"}, "<C-a>", "<Home>")
-vim.keymap.set({"n", "v", "i"}, "<C-e>", "<End>")
+-- leader + w will close the current buffer
+-- vim.keymap.set({"n", "v", "i"}, "<leader>w", vim.cmd.bd)
+vim.keymap.set({"n", "v", "i"}, "<C-a>", "<ESC>^")
+vim.keymap.set({"n", "v", "i"}, "<C-a>", "<ESC>I")
+vim.keymap.set({"n", "v", "i"}, "<C-e>", "<ESC>$")
+vim.keymap.set({"n", "v", "i"}, "<C-e>", "<ESC>A")
 
 vim.opt.relativenumber = true
 
